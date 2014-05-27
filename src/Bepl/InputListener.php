7e960e33351b42ce4b896f65d8f53cd8ddcdd089
Ilya S <public.ilya.s@gmail.com>
2014-05-27 22:05:15 +0700

    the very primitive prototype of listen()

--- src/Bepl/InputListener.php
+++ src/Bepl/InputListener.php
@@ -2,6 +2,21 @@
 
 class InputListener {
 
+    /**
+     * Listen to the user's input.
+     *
+     * @return void
+     */
+    public function listen()
+    {
+        $input = null;
 
+        while ($input !== 'exit')
+        {
+            $input = readline('>>> ');
+
+            readline_add_history($input);
+        }
+    }
 
 }
