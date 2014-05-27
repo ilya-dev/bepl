286ce0882ad73dbfd330c842f3ade9071b101d52
Ilya S <public.ilya.s@gmail.com>
2014-05-28 00:17:02 +0700

    Add Dumper

---
+++ specs/Spec/Bepl/DumperSpec.php
@@ -0,0 +1,12 @@
+<?php namespace Spec\Bepl;
+
+use PhpSpec\ObjectBehavior;
+
+class DumperSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Bepl\Dumper');
+    }
+
+}
