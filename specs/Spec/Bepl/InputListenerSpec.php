0d395e80aeed19c9066a3203d473f03ef41f5b4e
Ilya S <public.ilya.s@gmail.com>
2014-05-27 22:50:01 +0700

    rewrite the specifications to reflect the changes in InputListener

--- specs/Spec/Bepl/InputListenerSpec.php
+++ specs/Spec/Bepl/InputListenerSpec.php
@@ -1,18 +1,39 @@
 <?php namespace Spec\Bepl;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+use Bepl\HistoryManager;
 
 class InputListenerSpec extends ObjectBehavior {
+
+    function let(HistoryManager $manager)
+    {
+        $this->beConstructedWith($manager);
+    }
 
     function it_is_initializable()
     {
         $this->shouldHaveType('Bepl\InputListener');
     }
 
-    function it_listens_to_the_users_input()
+    function it_listens_to_the_users_input(HistoryManager $manager)
     {
+        $manager->load()->shouldBeCalled();
+        $manager->save()->shouldBeCalled();
+
+        $manager->add('foo')->shouldBeCalled();
+        $manager->add('exit')->shouldBeCalled();
+
         $this->listen();
     }
 
 }
+
+namespace Bepl;
+
+function readline($prompt)
+{
+    static $input = ['foo', 'exit'];
+
+    return array_shift($input);
+}
+
