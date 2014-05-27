ec18ac2c1b4507c1c5b9963a8344c3e732739547
Ilya S <public.ilya.s@gmail.com>
2014-05-27 22:15:10 +0700

    outline HistoryManager's responsibilities

--- src/Bepl/HistoryManager.php
+++ src/Bepl/HistoryManager.php
@@ -2,7 +2,36 @@
 
 class HistoryManager {
 
+    /**
+     * Add a line to the history.
+     *
+     * @param string $line
+     * @return void
+     */
+    public function add($line)
+    {
 
+    }
+
+    /**
+     * Load the history from a file.
+     *
+     * @return void
+     */
+    public function load()
+    {
+
+    }
+
+    /**
+     * Write the history to a file.
+     *
+     * @return void
+     */
+    public function save()
+    {
+
+    }
 
 }
 
