fe7d9823157cb62ca30c448753f431e890bef683
Ilya S <public.ilya.s@gmail.com>
2014-06-01 12:34:43 +0700

    add empty spec for NotationParser

---
+++ specs/Spec/Bepl/NotationParserSpec.php
@@ -0,0 +1,17 @@
+<?php namespace Spec\Bepl;
+
+use PhpSpec\ObjectBehavior;
+
+class NotationParserSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Bepl\NotationParser');
+    }
+
+    function it_parses_a_notation_into_an_array()
+    {
+
+    }
+
+}
---
+++ src/Bepl/NotationParser.php
@@ -0,0 +1,16 @@
+<?php namespace Bepl;
+
+class NotationParser {
+
+    /**
+     * Parse a given notation into an array.
+     *
+     * @param string $notation
+     * @return array
+     */
+    public function parse($notation)
+    {
+
+    }
+
+}
